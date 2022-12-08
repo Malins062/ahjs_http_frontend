063cea51dd98ea2491da32257b5a66b8ff0708ad
Malins062 <6328673@gmail.com>
2022-12-08 12:55:19 +0300

    Html complete

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,57 @@
 import HelpDeskWidget from "../widgets/helpdesk/helpdesk";
 
-const subscribeWidget = document.querySelector('[data-widget=subscribe]');
-const subscribeForm = subscribeWidget.querySelector('[data-id=subscribe-form]');
-const nameInput = subscribeWidget.querySelector('[data-id=name]');
-const phoneInput = subscribeWidget.querySelector('[data-id=phone]');
+// Адрес сервера
+const URL_SERVER = 'localhost:7070';
 
-subscribeForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-});
+// Ticket {
+//   id // идентификатор (уникальный в пределах системы)
+//   name // краткое описание
+//   status // boolean - сделано или нет
+//   created // дата создания (timestamp)
+// }
+
+// TicketFull {
+//   id // идентификатор (уникальный в пределах системы)
+//   name // краткое описание
+//   description // полное описание
+//   status // boolean - сделано или нет
+//   created // дата создания (timestamp)
+// }
+
+// Пример локального списка задач
+const tasksList = {
+  title: 'Список текущих задач',
+  items: [
+    {
+      name: 'Поменять краску в принтере, каб.404',
+      description: 'Закончилась краска в принтере, модель Epson-CJ300',
+      status: true,
+      created: '10.03.2022 08:40'
+    },
+    {
+      name: 'Переустановить OC Linux, каб.204',
+      description: 'Слетела операционная система, требуется переустановка',
+      status: true,
+      created: '22.11.2022 13:10'
+    },
+    {
+      name: 'Установить обновление KB-32565, каб.6',
+      description: 'Вышло критическое обновление Windows-10, требуется установка обновления',
+      status: false,
+      created: '08.12.2022 10:00'
+    },
+  ]
+};
+
+
+const helpDeskWidget = new HelpDeskWidget(document.querySelector('#widget-container'), null, tasksList);
+helpDeskWidget.bindToDOM();
+
+// const subscribeWidget = document.querySelector('[data-widget=subscribe]');
+// const subscribeForm = subscribeWidget.querySelector('[data-id=subscribe-form]');
+// const nameInput = subscribeWidget.querySelector('[data-id=name]');
+// const phoneInput = subscribeWidget.querySelector('[data-id=phone]');
+
+// subscribeForm.addEventListener('submit', (evt) => {
+//   evt.preventDefault();
+// });
--- src/widgets/helpdesk/helpdesk.js
+++ src/widgets/helpdesk/helpdesk.js
@@ -1,17 +1,38 @@
 import './helpdesk.css';
+import { v4 as uuidv4 } from 'uuid';
+
 
 export default class HelpDeskWidget {
-  constructor(parentEl, urlServer) {
+  constructor(parentEl, urlServer, tasksList = []) {
     this.parentEl = parentEl;
     this.urlServer = urlServer;
+    this.tasksList = tasksList;
   }
 
-  static itemHTML(itemText) {
+  static itemHTML(item) {
     const id = uuidv4();
     const html = `
         <li class="tasks__item list-group-item mb-2" draggable="true" data-id="${id}">
-          ${itemText}
-          <div class="item__close hidden" title="Удалить задачу">&#10005;</div>
+          <div class="row d-flex">
+            <div class="col-md-1">
+              <div class="form-check">
+                <input class="item__status form-check-input" type="checkbox" value="" id="flexCheckChecked" ${item.status ? 'checked' : ''}>
+              </div>
+            </div>
+            <div class="col-md-7 d-flex justify-content-start">
+              <h5 class="item__name mb-0">${item.name}</h5>
+            </div>
+            <div class="col-md-3 d-flex justify-content-center">
+              <h5 class="item__name mb-0">${item.created}</h5>
+            </div>
+            <div class="col-md-1 d-flex justify-content-end">
+              <button class="item__edit btn btn-success btn-sm" title="Редактировать задачу">&#10009;</button>
+              <button class="item__delete btn btn-danger btn-sm ms-1" title="Удалить задачу">&#10005;</button>
+          </div>         
+          <div class="col-md-1"></div>
+          <div class="item__description col-md-7 hidden">
+            <p>${item.description}</p>
+          </div>
         </li>`;
     return {
       innerHTML: html,
@@ -21,41 +42,35 @@ export default class HelpDeskWidget {
 
   static itemsHTML(items) {
     let html = '';
-    items.forEach((taskText) => {
-      html += this.itemHTML(taskText).innerHTML;
+    items.forEach((item) => {
+      html += this.itemHTML(item).innerHTML;
     });
     return html;
   }
 
   static tasksListHTML(tasksList) {
     return `
-      <div class="col-md-4 h-100 py-2">        
+      <div class="col-md-12 h-100 p-2">        
         <div class="tasks__card card" data-id="${tasksList.id}">
-            <div class="tasks__header card-header p-2">
-                <h5 class="tasks__title mb-0">${tasksList.title}</h5>
+          <div class="tasks__header card-header py-2">
+            <div class="row d-flex">
+              <div class="col-md-9 d-flex justify-content-start">
+                <h5 class="tasks__title mb-0 align-items-left">${tasksList.title}</h5>
+              </div>
+              <div class="col-md-3 d-flex justify-content-end">
+                <button class="tasks__add btn btn-success btn-sm" 
+                  title="Добавить новую задачу">
+                  &#10009; Добавить тикет
+                </button>
+              </div>
             </div>
+          </div>
 
-            <div class="tasks__body card-body h-100 p-2" data-mdb-perfect-scrollbar="true">
-                <ul class="tasks__list list-group">
-                    ${TasksListWidget.itemsHTML(tasksList.items)}
-                </ul>
-            </div>
-
-            <div class="tasks__footer card-footer text-start p-2">
-                <div class="item__add">&#10009; Добавить новую карточку</div>
-                <div class="item__card hidden">
-                  <form class="form-outline mb-2">
-                    <textarea class="new__item__text form-control mb-2" type="submit"
-                      placeholder="Введите текст карточки"></textarea>
-                    <button class="new__item__add btn btn-success btn-sm" title="Добавить новую задачу">
-                      Добавить
-                    </button>
-                    <button class="new__item__close btn btn-transparent btn-sm" title="Закрыть окно добавления">
-                      &#10005;
-                    </button>
-                  </form>
-                </div>
-            </div>
+          <div class="tasks__body card-body h-100 p-2" data-mdb-perfect-scrollbar="true">
+              <ul class="tasks__list list-group">
+                  ${HelpDeskWidget.itemsHTML(tasksList.items)}
+              </ul>
+          </div>
         </div>
       </div>
     `;
@@ -87,6 +102,10 @@ export default class HelpDeskWidget {
 
   static get delItemSelector() {
     return '.item__close';
+  }
+
+  static get descriptionItemSelector() {
+    return '.item__description';
   }
 
   static get delItemClass() {
@@ -122,11 +141,86 @@ export default class HelpDeskWidget {
   bindToDOM() {
     // Отрисовка HTML
     this.parentEl.innerHTML = '';
-    this.tasksList.forEach((tasksList) => {
-      this.parentEl.innerHTML += TasksListWidget.tasksListHTML(tasksList);
+    if (!this.urlServer) {
+      this.tasksList.id = uuidv4();
+      this.parentEl.innerHTML += HelpDeskWidget.tasksListHTML(this.tasksList);
+    }
+
+    this.initEvents();
+  }
+
+  initEvents() {
+    const tasksListItems = this.parentEl.querySelector(HelpDeskWidget.listItemsSelector);
+
+    // // Отработка событий на добавлении новой карточки-задачи
+    // const showCardItem = tasksCard.querySelector(TasksListWidget.showCardSelector);
+    // const addNewItem = tasksCard.querySelector(TasksListWidget.addCardSelector);
+    // const closeCardItem = tasksCard.querySelector(TasksListWidget.closeCardSelector);
+
+    // showCardItem.addEventListener('click', (evt) => this.onClickShowCard(evt, tasksCard));
+    // addNewItem.addEventListener('click', (evt) => this.onClickAddCard(evt, tasksCard, tasksListItems));
+    // closeCardItem.addEventListener('click', (evt) => this.onClickCloseCard(evt, tasksCard));
+
+    // this.onDragEnter = this.onDragEnter.bind(this);
+    // this.onDragLeave = this.onDragLeave.bind(this);
+    // this.onDrop = this.onDrop.bind(this);
+    // this.onDragOver = this.onDragOver.bind(this);
+    // tasksListItems.addEventListener('dragenter', this.onDragEnter);
+    // tasksListItems.addEventListener('dragleave', this.onDragLeave);
+    // tasksListItems.addEventListener('drop', this.onDrop);
+    // tasksListItems.addEventListener('dragover', this.onDragOver);
+
+    this.initItemsEvents(tasksListItems);
+  }
+
+  initItemsEvents(ul) {
+    // Отработка событий на каждой задаче из списка
+    const items = ul.querySelectorAll(HelpDeskWidget.itemSelector);
+    items.forEach((item) => this.initItemEvents(item));
+  }
+
+  initItemEvents(item) {
+    const descritpion = item.querySelector(HelpDeskWidget.descriptionItemSelector);
+    // Событие по клику задачу
+    item.addEventListener('click', (evt) => {
+      if (!descritpion) {
+        return;
+      }
+      descritpion.classList.toggle('hidden');
     });
 
-    // Добавление отслеживания событий на каждый список
-    this.tasksList.forEach((list) => this.initEvents(list.id));
+
+    // // Событие удаления задачи
+    // const closeButton = item.querySelector(TasksListWidget.delItemSelector);
+    // closeButton.addEventListener('click', () => {
+    //   item.remove();
+    //   this.saveItems();
+    // });
+
+    // // Событие входа в зону наведения курсора на задачу
+    // item.addEventListener('mouseover', (evt) => {
+    //   evt.preventDefault();
+    //   if (closeButton.classList.contains('hidden')) {
+    //     closeButton.classList.remove('hidden');
+    //   }
+    // });
+
+    // // Событие выхода из зоны наведения курсора на задачу
+    // item.addEventListener('mouseout', (evt) => {
+    //   evt.preventDefault();
+    //   const delButtons = document.querySelectorAll(TasksListWidget.delItemSelector);
+    //   delButtons.forEach((delButton) => {
+    //     if (delButton && !delButton.classList.contains('hidden')) {
+    //       delButton.classList.add('hidden');
+    //     }
+    //   });
+    // });
+
+    // this.onDrag = this.onDrag.bind(this);
+    // this.onDragStart = this.onDragStart.bind(this);
+    // this.onDragEnd = this.onDragEnd.bind(this);
+    // item.addEventListener('dragstart', this.onDragStart);
+    // item.addEventListener('dragend', this.onDragEnd);
   }
+
 }
